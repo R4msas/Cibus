4114372897de41eafe67f4a1dd5ea11f6160d9f0
joaopmour4 <joaopedrormoura1@gmail.com>
2023-05-23 23:20:52 -0300

    olha

---
+++ maven.Cibus/src/main/java/service/Service.java
@@ -0,0 +1,21 @@
+package service;
+
+import java.net.URISyntaxException;
+
+/**
+ * Interface service
+ * 
+ * @author danniel
+ *
+ * Última alteração 07/11/2020
+ */
+import spark.Request;
+import spark.Response;
+
+public interface Service {
+	public Object add(Request r, Response p) throws URISyntaxException;
+	public Object get(Request r, Response p) throws URISyntaxException;
+	public Object remove(Request r, Response p) throws URISyntaxException;
+	public Object update(Request r, Response p) throws Exception, Throwable;
+	public Object getAll(Request r, Response p) throws URISyntaxException;
+}
