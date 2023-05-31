eee99173874c80664fd9844280a4b0eb0f573a86
GustavoFirmino <gustavopessoa00719@gmail.com>
2023-05-30 22:01:59 -0300

    novos html

--- maven.Cibus/src/main/resources/public/produtos.js
+++ maven.Cibus/src/main/resources/public/produtos.js
@@ -1,69 +1,53 @@
 function carrega() {
-	var categoria = {
-    "1": "images/carneCibus.jpeg",
-    "2": "images/bebeidasCibus.jpeg",
-    "3": "images/arrozCibus.jpeg",
-    "4": "images/feijãoCibus.jpeg",
-    "5": "images/oleosCibus.jpeg",
-    "6": "images/macarraoCibus.jpeg",
-    "7": "images/sucosCibus.jpeg",
-    "8": "images/refrisCibus.jpeg",
-    "9": "images/bolachasCibus.jpeg",
-    "10": "images/paesCibus.jpeg",
-    "11": "images/laticiniosCibus.jpeg",
-    "12": "images/xampusCibus.jpeg",
-    "13": "images/sabaoCibus.jpg",
-    "14": "images/sabaoCibus.jpg",
-    "15": "images/cervejaCibus.jpeg",
-    "16": "images/farinhaCibus.jpeg",
-    "17": "images/cafeCibus.jpeg",
-    "18": "images/iogurtesCibus.jpeg",
-    "19": "images/leiteCibus.jpeg"
+  var categoria = {
+    "1": "Carne",
+    "2": "Bebidas",
+    "3": "Arroz",
+    "4": "Feijão",
+    "5": "Óleos",
+    "6": "Macarrão",
+    "7": "Suco",
+    "8": "Refrigerante",
+    "9": "Bolacha",
+    "10": "Pão",
+    "11": "Laticínios",
+    "12": "Shampoo",
+    "13": "Sabonete",
+    "14": "Sabão",
+    "15": "Cerveja",
+    "16": "Farinha",
+    "17": "Café",
+    "18": "Iogurtes",
+    "19": "Leite"
   };
-	
+
   let url = `/all`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      let str = '';
-      console.log(data);
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    let str = '';
+    console.log(data);
 
-      for (let i = 0; i < data.length; i++) {
-        let oferta = data[i];
-       
-        str += `
-          <table>
-      <tr>
-        <th>${oferta.descricao}</th>
-        <th>${oferta.preco}</th>
-        <th>${}</th>
-        <th>Ações</th>
-      </tr>
-      <tr>
-        <td>Produto 1</td>
-        <td>R$ 10.00</td>
-        <td>Categoria 1</td>
-        <td>
-          <button class="excluir-btn">Excluir</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Produto 2</td>
-        <td>R$ 15.00</td>
-        <td>Categoria 2</td>
-        <td>
-          <button class="excluir-btn">Excluir</button>
-        </td>
-      </tr>
-    </table>
-        `;
-      }
+    for (let i = 0; i < data.length; i++) {
+      let oferta = data[i];
+      let cate = categoria[oferta.tipoProduto];
 
-      // Colocar todos os cards dentro do contêiner
-      document.getElementById('tela').innerHTML = `
-        <div class="produtos-container">
-          ${str}
-        </div>
+      str += `
+        <tr>
+          <td>${oferta.descricao}</td>
+          <td>R$ ${oferta.preco}</td>
+          <td>${cate}</td>
+          <td>
+            <button class="excluir-btn">Excluir</button>
+          </td>
+        </tr>
       `;
-    });
+    }
+
+    // Atualizar o conteúdo da tabela com as linhas geradas dinamicamente
+    document.querySelector('#produtos-table tbody').innerHTML = str;
+  });
+
 }
+
+carrega();
